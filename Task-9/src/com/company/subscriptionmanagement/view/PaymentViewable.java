581bc6e37b5262627ade5d44f8b41ceb5fbf67af
GJ-16 <12jagadeesh4567@gmail.com>
2021-10-15 20:24:23 +0530

    update

--- Task-9/src/com/company/subscriptionmanagement/view/PaymentViewable.java
+++ Task-9/src/com/company/subscriptionmanagement/view/PaymentViewable.java
@@ -6,7 +6,7 @@ public interface PaymentViewable {
 
     void view();
     default void getPaymentMethod(PaymentController paymentController){
-        int option = GetValues.getIntegerValue("1.Use saved payment details (you have already used our service and we have your payment details) or any other key to " +
+        int option = GetValues.getIntegerValue("1.Use saved payment details (you have already used our service and we have saved your card details) or any other key to " +
                 "enter New Payment details", "Select a valid option");
         paymentController.setOption(option);
     }
